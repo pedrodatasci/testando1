0890396a6152dc1902d56eae65cda33e326aad4e
pedro.datasci <pedro.datasci@gmail.com>
2022-09-21 10:06:09 -0300

    funcao divisao isadjilaksdm

--- teste.py
+++ teste.py
@@ -4,5 +4,8 @@ def soma(n1: int, n2: int) -> int:
 def subtrai(n1: int, n2: int) -> int:
     return n1 - n2
 
+def divisao(num1: int, num2: int):
+    return num2/num1
+
 if __name__ == '__main__':
     print(soma(1,1))
