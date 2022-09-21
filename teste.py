8fb658aba0e946601b42881b4a8ba743bb6c811f
pedro.datasci <pedro.datasci@gmail.com>
2022-09-21 09:52:24 -0300

    adicionando ordem na funcao de subtracao

--- teste.py
+++ teste.py
@@ -1,8 +1,9 @@
 def soma(n1: int, n2: int) -> int:
     return n1 + n2
 
-def subtrai(n1: int, n2: int) -> int:
-    return n1 - n2
+def subtrai(n1: int, n2: int, ordem:int) -> int:
+    n = [n1, n2]
+    return max(n1) - min(n2)
 
 if __name__ == '__main__':
     print(soma(1,1))
