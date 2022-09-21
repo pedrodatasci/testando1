4a608b5ff7528ae9356538a85746ffbe870454d8
pedro.datasci <pedro.datasci@gmail.com>
2022-09-21 10:04:38 -0300

    funcao de divisao com dois ints

--- teste.py
+++ teste.py
@@ -4,5 +4,8 @@ def soma(n1: int, n2: int) -> int:
 def subtrai(n1: int, n2: int) -> int:
     return n1 - n2
 
+def divide(n1: int, n2:int):
+    return n1/n2
+
 if __name__ == '__main__':
     print(soma(1,1))
